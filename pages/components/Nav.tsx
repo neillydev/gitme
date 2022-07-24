c94f749697b4afd71ec230154848aa71795e5765
neillycubed <neillybusiness3@gmail.com>
2022-07-23 23:29:03 -0700

    create Nav component

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -1,9 +1,35 @@
+import Link from 'next/link';
 import React from 'react';
+import styles from '../../styles/Nav.module.scss';
+import NavItem from './NavItem';
+import Github from '../../public/github.svg';
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+    return (
+        <header className={`${styles.navHeader}`}>
+            <nav className={styles.nav}>
+                <div className={styles.navLogo}>
+                    <a href='/'>
+                        GitMe_
+                    </a>
+                </div>
+                <div className={styles.navList}>
+                    <ul>
+                        {/* <NavItem destination='/' itemText='' /> */}
+                    </ul>
+                </div>
+                <div className={styles.navEnd}>
+                    <a href='https://github.com/neillydev/gitme' target='_blank'>
+                        <span className={styles.navGH}>
+                            {/* <img src="/github.svg" alt="GitMe Repository" /> */}
+                            <Github />
+                            GitHub
+                        </span>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Nav
