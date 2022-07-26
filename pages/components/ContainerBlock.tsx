dad96f6c8c23115331cdbd37f24b79f90515547b
neillycubed <neillybusiness3@gmail.com>
2022-07-25 18:21:37 -0700

    add Nav component to all pages

--- pages/components/ContainerBlock.tsx
+++ pages/components/ContainerBlock.tsx
@@ -33,8 +33,8 @@ export default function ContainerBlock({ children, ...customMeta }: any) {
                 <link rel="icon" type="image/png" href="/gitme.ico" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" 
-                rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+                    rel="stylesheet" />
                 <meta property="og:type" content={meta.type} />
                 <meta property="og:site_name" content="Git Me" />
                 <meta property="og:description" content={meta.description} />
@@ -49,13 +49,9 @@ export default function ContainerBlock({ children, ...customMeta }: any) {
                     <meta property="article:published_time" content={meta.date} />
                 )}
             </Head>
-            <div className="containerBlock">
-                <Nav />
-                {/* {navOpen ? <HamburgerMenu /> : null} */}
-                <main className={styles.mainContainer}>
-                    {children}
-                </main>
-            </div>
+            <main className={styles.mainContainer}>
+                {children}
+            </main>
         </div>
     );
 }
