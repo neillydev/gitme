51925a879557a14313e7bcefa9b88d0aaaaaf121
neillycubed <neillybusiness3@gmail.com>
2022-07-25 18:21:57 -0700

    create dynamic portfolio page

---
+++ pages/[user_id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/Portfolio.module.scss';
+
+
+const Portfolio = () => {
+  return (
+    <div className={styles.portfolioContainer}></div>
+  )
+}
+
+export default Portfolio
