d6acf3dc9cc98352e5b2a8f5d2b73f9f1c7593aa
neillycubed <neillybusiness3@gmail.com>
2022-07-30 01:25:03 -0700

    create portfolio component structure

--- pages/[user_id].tsx
+++ pages/[user_id].tsx
@@ -4,7 +4,19 @@ import styles from '../styles/Portfolio.module.scss';
 
 const Portfolio = () => {
   return (
-    <div className={styles.portfolioContainer}></div>
+    <div className={styles.portfolioContainer}>
+      <aside className={styles.portfolioLeft}>
+        <div className={styles.profileContainer}>
+          <div className={styles.profileWrapper}>
+
+            <div className={styles.portfolioImg}></div>
+            <div className={styles.name}>Vernon Neilly III</div>
+            <div className={styles.username}><span />neillydev</div>
+          </div>
+        </div>
+      </aside>
+      <div className={styles.portfolioRight}></div>
+    </div>
   )
 }
 
