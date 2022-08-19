fdaa8894682fb2ecefdeaea4fea3014eb65d97d6
neillycubed <neillybusiness3@gmail.com>
2022-08-18 19:48:10 -0700

    create Project component for projects

---
+++ pages/components/Project.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from '../../styles/Project.module.scss';
+
+const Project = () => {
+  return (
+    <div className={styles.projContainer}>
+        <div className={styles.project}>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Project
