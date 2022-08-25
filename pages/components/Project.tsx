2f72f5e5b641def5cf6f7937b91c78a797ac7dfa
neillycubed <neillybusiness3@gmail.com>
2022-08-24 22:18:20 -0700

    create project card design

--- pages/components/Project.tsx
+++ pages/components/Project.tsx
@@ -2,14 +2,22 @@ import React from 'react'
 
 import styles from '../../styles/Project.module.scss';
 
+type Props = {
+  profile_img: string;
+  name: string;
+  username: string;
+  bio: string;
+
+};
+
 const Project = () => {
   return (
     <div className={styles.projContainer}>
       <div className={styles.projHeader}>
         <h1>gitme</h1>
       </div>
-      <div className={styles.project}>
-
+      <div className={styles.projectOverlay}>
+        <img className={styles.projectImg} src="https://i.imgur.com/95sfhtr.png"></img>
       </div>
     </div>
   )
