6d9a556b45f83bc24bfffa6bd2844c32afb990ed
neillycubed <neillybusiness3@gmail.com>
2022-08-24 22:30:55 -0700

    add profile image w/ styling

--- pages/[user_id].tsx
+++ pages/[user_id].tsx
@@ -10,7 +10,9 @@ const Portfolio = () => {
       <aside className={styles.portfolioLeft}>
         <div className={styles.profileContainer}>
           <div className={styles.profileWrapper}>
-            <div className={styles.portfolioImg}></div>
+            <div className={styles.portfolioImg}>
+              <img src="https://avatars.githubusercontent.com/u/51303046?v=4" alt="profile picture" />
+            </div>
             <div className={styles.portfolioBio}>
               <div className={styles.name}>Vernon Neilly III</div>
               <div className={styles.username}><span />@neillydev</div>
