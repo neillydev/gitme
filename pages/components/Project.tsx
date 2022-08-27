4a6c9e04c4295efdd3eacc27a0fd6f08f598b5e2
neillycubed <neillybusiness3@gmail.com>
2022-08-27 14:45:52 -0700

    change structure of project component

--- pages/components/Project.tsx
+++ pages/components/Project.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ExtSvg from '../../public/extlink.svg';
 
 import styles from '../../styles/Project.module.scss';
 
@@ -15,6 +16,14 @@ const Project = () => {
     <div className={styles.projContainer}>
       <div className={styles.projHeader}>
         <h1>gitme</h1>
+        <h2>The Modern Developer's Portfolio</h2>
+      </div>
+      {/* <div className={styles.projectBody}>
+        <h2>The Modern Developer's Portfolio</h2>
+        <ExtSvg />
+      </div> */}
+      <div className={styles.projectFooter}>
+        <a href='http://localhost:3000' target='_blank'><ExtSvg /></a>
       </div>
       <div className={styles.projectOverlay}>
         <img className={styles.projectImg} src="https://i.imgur.com/95sfhtr.png"></img>
