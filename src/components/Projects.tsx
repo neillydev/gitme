ff3ded957e6b8b59b3caaea09138720e9b6ed766
neillycubed <neillybusiness3@gmail.com>
2022-08-29 00:36:35 -0700

    create not found clause if no projects are found

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
 import SearchSVG from '../../public/search.svg';
+import ConfusedSVG from '../../public/confused.svg';
 
 import styles from '../../styles/Projects.module.scss';
 
 const Projects = () => {
+    const [projectsArray, setProjectsArray] = useState([]);
 
     const handleSearch = () => {
 
@@ -16,13 +18,24 @@ const Projects = () => {
 
     return (
         <div className={styles.projectsContainer}>
-            <div className={styles.searchContainer}>
+            <div className={styles.projectsWrapper}>
                 <form className={styles.searchForm} onSubmit={handleSearch}>
                     <div className={styles.searchBar}>
                         <span><SearchSVG /></span>
                         <input type="text" name="search" placeholder='Search projects...' onChange={handleChange} />
                     </div>
                 </form>
+                {/* <ul className={styles.projectsList}></ul> */}
+                <div className={styles.projectsBody}>
+                    {
+                        projectsArray.length > 0 ? null
+                            :
+                            <div className={styles.nothingFound}>
+                                <ConfusedSVG />
+                                <h1>Sorry, no projects found</h1>
+                            </div>
+                    }
+                </div>
             </div>
         </div>
     );
