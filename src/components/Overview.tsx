15b90ea3547b27f091e2231a0270aebe5aed6182
neillycubed <neillybusiness3@gmail.com>
2022-08-28 22:54:59 -0700

    create Overview component

---
+++ src/components/Overview.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../../styles/Portfolio.module.scss';
+import Feed from './Feed';
+import Project from './Project';
+
+const Overview = () => {
+  return (
+    <><div className={styles.projectsSection}>
+    <a href='https://github.com/neillydev/gitme' target='_blank'><Project /></a>
+  </div>
+  <Feed /> </>
+  )
+}
+
+export default Overview
