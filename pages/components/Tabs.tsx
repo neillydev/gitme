6257f8bc0483d9ce7912084c070d13d499ac1756
neillycubed <neillybusiness3@gmail.com>
2022-08-28 22:28:54 -0700

    add tab selection

--- pages/components/Tabs.tsx
+++ pages/components/Tabs.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../styles/Tabs.module.scss';
 
 const Tabs = () => {
+  const [selected, setSelected] = useState(0);
+
   return (
     <div className={styles.tabContainer}>
         <ul className={styles.tabList}>
-            <li className={`${styles.tab} ${styles.tabSelected}`}>Overview</li>
-            <li className={styles.tab}>Projects</li>
-            <li className={styles.tab}>Experience</li>
-            <li className={styles.tab}>Contact</li>
+            <li className={`${styles.tab} ${selected === 0 && styles.tabSelected}`} onClick={() => selected !== 0 && setSelected(0)}>Overview</li>
+            <li className={`${styles.tab} ${selected === 1 && styles.tabSelected}`} onClick={() => selected !== 1 && setSelected(1)}>Projects</li>
+            <li className={`${styles.tab} ${selected === 2 && styles.tabSelected}`} onClick={() => selected !== 2 && setSelected(2)}>Experience</li>
+            <li className={`${styles.tab} ${selected === 3 && styles.tabSelected}`} onClick={() => selected !== 3 && setSelected(3)}>Contact</li>
         </ul>
     </div>
   )
