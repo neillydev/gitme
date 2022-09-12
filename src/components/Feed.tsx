49b940ab41c3513f98f9f58ac555519327b9d59a
neillycubed <neillybusiness3@gmail.com>
2022-09-11 23:13:35 -0700

    add commits state and API handler

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import styles from '../../styles/Feed.module.scss';
 
 const Feed = () => {
+    const [commits, setCommits] = useState([]);
+
+    const handleLoad = () => {
+        axios.get('').then((data: any) => {
+            
+        });
+    }
+
+    useEffect(() => {
+        if(!commits) handleLoad();
+    }, []);
+
     return (
         <div className={styles.feedSection}>
             <div className={styles.feedContainer}>
