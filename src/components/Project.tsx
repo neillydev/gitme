2708c62078454afbe4a32e936aa1a769c8a5f08c
neillycubed <neillybusiness3@gmail.com>
2022-09-18 21:46:36 -0700

    add dynamic API calls for featured projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
 import ExtSvg from '../../public/extlink.svg';
 
 import styles from '../../styles/Project.module.scss';
@@ -12,11 +14,40 @@ type Props = {
 };
 
 const Project = () => {
+  const router = useRouter();
+  const { user_id } = router.query;
+  const [featuredProject, setFeaturedProject] = useState<any>();
+  const [image, setImage] = useState('');
+
+  const handleLoad = async () => {
+    await axios.get(`/api/portfolio/projects/${user_id}`).then(async ({data: { items }}) => {
+      if(user_id === '@neillydev') {
+        const [repo] = items.filter((item: any) => item.name === 'gitme');
+        setFeaturedProject(repo);
+
+        await axios.get(`/api/portfolio/projects/${user_id}/readme/${repo.name}`).then(({data}: any) => {
+          const content = atob(data.content);
+          const images = content.match(/(?<=src=").+?(?=")/g) || '';
+
+          setImage(images[1]);
+        });
+      }
+      else{
+
+      }
+    });
+  };
+
+  useEffect(() => {
+    if(!user_id) return;
+    handleLoad();
+  }, [user_id]);
+
   return (
     <div className={styles.projContainer}>
       <div className={styles.projHeader}>
-        <h1>gitme</h1>
-        <h2>The Modern Developer's Portfolio</h2>
+        <h1>{featuredProject?.name}</h1>
+        <h2>{featuredProject?.description}</h2>
       </div>
       <div className={styles.projectFooter}>
         <ExtSvg>
@@ -25,7 +56,7 @@ const Project = () => {
         {/*  */}
       </div>
       <div className={styles.projectOverlay}>
-        <img className={styles.projectImg} src="https://i.imgur.com/95sfhtr.png"></img>
+        <img className={styles.projectImg} src={image}></img>
       </div>
     </div>
   )
