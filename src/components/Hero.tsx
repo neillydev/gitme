554607e229662dbc3c38e9aac25e44a94cf89302
neillycubed <neillybusiness3@gmail.com>
2022-09-20 05:06:36 -0700

    create Preview SVG for Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from '../../styles/Hero.module.scss';
 
+import Preview from '../../public/Preview_Light.svg';
+
 const Hero = () => {
   return (
     <section className={styles.heroSection}>
@@ -11,7 +13,11 @@ const Hero = () => {
         <div className={styles.heroSubheader}>
           <h4>No signup required.</h4>
         </div>
-        <div className={styles.heroGrid}></div>
+        <div className={styles.heroGrid}>
+          <div className={styles.previewWrapper}>
+            <Preview />
+          </div>
+        </div>
       </div>
     </section>
   )
