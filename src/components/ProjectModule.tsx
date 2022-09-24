c02a327515364e7635077a23b57cb8ea81c3596c
neillycubed <neillybusiness3@gmail.com>
2022-09-23 22:14:43 -0700

    create ProjectModule props

--- src/components/ProjectModule.tsx
+++ src/components/ProjectModule.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styles from '../../styles/ProjectModule.module.scss';
 
-const ProjectModule = () => {
+type Props = {
+  name: string;
+  description: string;
+};
+
+const ProjectModule = ({ 
+  name,
+  description
+ }: Props) => {
   return (
     <li className={styles.modContainer}>
-      <div className={styles.modWrapper}></div>
+      <div className={styles.modWrapper}>
+        <div className={styles.modHead}>
+          
+        </div>
+        <div className={styles.modHeader}>
+          
+        </div>
+        <div className={styles.modBody}>
+
+        </div>
+      </div>
     </li>
   )
 }
