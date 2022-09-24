fcccfe8938f37d412bfbe4e2d197917e3fabec53
neillycubed <neillybusiness3@gmail.com>
2022-09-23 23:48:15 -0700

    add props and github SVG to project module

--- src/components/ProjectModule.tsx
+++ src/components/ProjectModule.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import GithubSmSVG from '../../public/github_sm.svg';
+
 import styles from '../../styles/ProjectModule.module.scss';
 
 type Props = {
@@ -14,14 +17,13 @@ const ProjectModule = ({
     <li className={styles.modContainer}>
       <div className={styles.modWrapper}>
         <div className={styles.modHead}>
-          
+          <GithubSmSVG />
         </div>
         <div className={styles.modHeader}>
-          
+          <h1>{name}</h1>
+          <h2>{description}</h2>
         </div>
-        <div className={styles.modBody}>
-
-        </div>
+        <div className={styles.modFooter}></div>
       </div>
     </li>
   )
