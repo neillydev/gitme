6b8ba04f1b5810334d31efa3c2b876164313a774
neillycubed <neillybusiness3@gmail.com>
2022-10-15 03:21:49 -0700

    add profile search functionality to Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import styles from '../../styles/Hero.module.scss';
 
 const Hero = () => {
+  const router = useRouter();
+  const [path, setPath] = useState('/@neillydev');
+
+  const handleChange = ((e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPath(`/@${e.currentTarget.value}`);
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    router.push({
+      pathname: path,
+    })
+  }
   return (
     <section className={styles.heroSection}>
       <div className={styles.heroContainer}>
         <div className={styles.heroCaption}>
-            <h1>The <span>Modern</span> Developer's Portfolio</h1>
-            <h2 className={styles.heroSubheader}>No signup required.</h2>
+          <h1>The <span>Modern</span> Developer's Portfolio</h1>
+          <h2 className={styles.heroSubheader}>No signup required.</h2>
         </div>
         <div className={styles.heroSearch}>
-            <input type="text" placeholder='Your Github username'/>
+          <form className={styles.navCenter} onSubmit={handleSubmit}>
+            <input type="text" placeholder='Your Github username' onChange={handleChange} />
             <button>Git Me</button>
+          </form>
         </div>
       </div>
     </section>
