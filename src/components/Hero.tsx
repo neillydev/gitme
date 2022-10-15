1fd9e28455e051840352e1ee43199ab0cf36e875
neillycubed <neillybusiness3@gmail.com>
2022-10-15 03:19:01 -0700

    refactor Hero HTML structure

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import styles from '../../styles/Hero.module.scss';
 
-import PreviewXL from '../../public/Preview_Light.svg';
-import PreviewLarge from '../../public/Preview_Light_1600.svg';
-import Preview from '../../public/Preview_Light_1290.svg';
-
 const Hero = () => {
   return (
     <section className={styles.heroSection}>
       <div className={styles.heroContainer}>
-        <div className={styles.heroHeader}>
-          <h1>The <span>Modern</span> Developer's Portfolio</h1>
+        <div className={styles.heroCaption}>
+            <h1>The <span>Modern</span> Developer's Portfolio</h1>
+            <h2 className={styles.heroSubheader}>No signup required.</h2>
         </div>
-        <div className={styles.heroSubheader}>
-          <h4>No signup required.</h4>
-        </div>
-        <div className={styles.heroGrid}>
-          <div className={styles.previewWrapper}>
-            <PreviewXL className={styles.previewXL} />
-            <PreviewLarge className={styles.previewLarge} />
-            <Preview className={styles.preview} />
-          </div>
+        <div className={styles.heroSearch}>
+            <input type="text" placeholder='Your Github username'/>
+            <button>Git Me</button>
         </div>
       </div>
     </section>
